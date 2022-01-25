c67fcbc0ee9114307e33104d632453913fb2c04b
Tom Wilson <tom@hyper.io>
2022-01-25 14:06:20 +0000

    updated deno ext with types

--- deno/mod.ts
+++ deno/mod.ts
@@ -1,3 +1,7 @@
+import { R } from './deps.ts'
+
+const { prop, mergeRight } = R
+
 interface Count {
   count: number
 }
@@ -6,14 +10,14 @@ interface HyperCache {
   get: (key: string) => Promise<Count> | Promise<{ ok: boolean }>
 }
 
-type HyperCounter = (cache: HypeCache, key: string) => Promise<Number> | Promise<Error>
+type HyperCounter = (cache: HyperCache, key: string) => Promise<Number> | Promise<Error>
 const get: HyperCounter = (cache, key) => cache.get(key).then(prop(key)).catch(() => new Error(`${key} not found.`))
 
 export const counter = (hyper) =>
   mergeRight(hyper, {
     ext: {
       counter: {
-        get: 
+        get: (key: string) => get(hyper.cache, key)
       }
     }
   })
